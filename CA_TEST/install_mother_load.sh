3426edb294b2b83edd3bd4b99e89a8b226c0026f
Clement32112 <totoobieke@gmail.com>
2023-06-27 18:05:30 +0100

    .

---
+++ CA_TEST/install_mother_load.sh
@@ -0,0 +1,11 @@
+npx create-expo-app -t expo-template-blank-typescript amakaCrudMobileAppv48plus
+cd amakaCrudMobileAppv48plus
+npx expo install react-dom react-native-web @expo/webpack-config
+npx expo install typeorm reflect-metadata
+npx expo install expo-sqlite
+npx expo install @react-native-async-storage/async-storage
+npx expo install @rneui/themed @rneui/base
+npx expo install react-native-safe-area-context
+npx expo install react-native-vector-icons
+npx expo install @react-native-picker/picker
+npx expo install @react-native-community/datetimepicker
